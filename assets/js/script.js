9b4a1b40beb798294cdd581b0775bd99ca469013
wilgru <william.gruszka@gmail.com>
2022-04-16 09:41:52 +1000

    added event listener, clock and a function to show round info when pay button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
+// elements
 const gifContainers = document.querySelectorAll(".gif-container")
+const headerRow = document.getElementById("header-row")
+const headerTitle = document.getElementById("header-title")
+const timeInfo = document.querySelector(".time-info")
+const scoreInfo = document.querySelector(".score-info")
+const guessform = document.getElementById("guess-form")
+const guessInput = document.getElementById("guess-input")
+const headerChildren = {
+	title: headerRow.children[0],
+	roundInfoSimple: headerRow.children[1],
+	roundInfoAdvanced: headerRow.children[2]
+}
+//this is an array of the elements in the headerchildren object. It exists so that it can be spread out as arguenments for the fade in/out functions. 
+//this is done so that the switch header display function only worries about changing the element currently displayed, but does not interefere with the opacity at all.
+const headerChildrenArrayOfEls = Object.values(headerChildren)
 
+// global static vars
+const numGifContainers = gifContainers.length
+const totalTime = 6000;
 const buttons = {
 	home: [
 		{key: "playBtn", text: "Play", id: "play-btn"},
@@ -45,12 +63,12 @@ const words = [
 	"animal",
 	"music"
 ]
-const numGifContainers = gifContainers.length
-const time = 60000;
 
+// game play vars
 let score = 0
 let dynamicBlockElements = {}
 let currentRound = {}
+let currentRoundWord = ""
 let nextRound = {}
 let nextRoundPrepared = false
 
@@ -66,15 +84,36 @@ init()
 function startGame() {
 	fadeGifsOut()
 
-	// wait an inital 500ms (for the fade out to complete) then begin to wait for next round to be ready to load
+	// wait an inital 500ms (for the fade out to complete) then fade out the header, then begin to wait for next round to be ready to load
 	setTimeout(() => {
+		elementFadeOut(...headerChildrenArrayOfEls) 
 		blockAndWait(hideButtons, fadeGifsIn, startCountdown, loadNextRound, prepareNextRound)
+
+		// wait 250ms for buttons to fade out then then change header
+		setTimeout(()=>{
+			switchHeaderDisplay("roundInfoSimple")
+
+			// wait 50ms for header to be changed then fade in new header
+			setTimeout(()=>{
+				elementFadeIn(...headerChildrenArrayOfEls)
+				elementFadeIn(guessInput)
+			}, 50)
+		}, 250)
 	}, 500);
 }
 
 // start the game countdown
 function startCountdown() {
+	timeLeft = totalTime
 
+	const timer = setInterval(() => {
+		timeLeft--
+		timeInfo.innerHTML = (timeLeft/100).toFixed(2)
+
+		if (timeLeft === 0){
+			clearInterval(timer)
+		}
+	}, 10);
 }
 
 // execute parsed function once the next round is finished preparing
@@ -128,6 +167,15 @@ function showButtons(btnSet) {
 		)
 }
 
+// choose which child of te header tag to display
+function switchHeaderDisplay(childIndex) {
+	const desiredEl = headerChildren[childIndex]
+	for (key in headerChildren) {
+		headerChildren[key].style.display = "none"
+	}
+	desiredEl.style.display = "flex"
+}
+
 // hide any buttons currently on the page
 function hideButtons() {
 	for (const btnKey in dynamicBlockElements) {
@@ -137,21 +185,25 @@ function hideButtons() {
 }
 
 // fade element in with box shadow
-function elementFadeIn(el) {
-	el.style.opacity = "100"
+function elementFadeIn(...els) {
+	els.forEach(el => {
+		el.style.opacity = "1"
 
-	setTimeout(() => {
-		el.style.boxShadow = "var(--dark) 5px 5px"
-	}, 100);
+		setTimeout(() => {
+			el.style.boxShadow = "var(--dark) 5px 5px"
+		}, 100);
+	})
 }
 
 // fade element out
-function elementFadeOut(el) {
-	el.style.boxShadow = "var(--dark) 0 0"
+function elementFadeOut(...els) {
+	els.forEach(el => {
+		el.style.boxShadow = "var(--dark) 0 0"
 
-	setTimeout(() => {
-		el.style.opacity = "0" 
-	}, 100);
+		setTimeout(() => {
+			el.style.opacity = "0" 
+		}, 100);
+	})
 }
 
 // cascade fade the gifs out
@@ -167,6 +219,8 @@ function fadeGifsIn() {
 // transition between round
 function roundTransition() {
 	fadeGifsOut()
+	score++
+	scoreInfo.innerHTML = `Score: ${score}`
 
 	// wait an inital 500ms (for the fade out to complete) then begin to wait for next round to be ready to load
 	setTimeout(() => {
@@ -195,8 +249,11 @@ function prepareNextRound() {
 
 // get everything all the gifs that were prepared and load them 
 function loadNextRound() {
+	currentRound = nextRound
+	currentRoundWord = currentRound.answer
+
 	gifContainers.forEach(
-		(item, key) => item.style.backgroundImage = `url("${nextRound.urls[key].images.original.url}")`
+		(item, key) => item.style.backgroundImage = `url("${currentRound.urls[key].images.original.url}")`
 	)
 }
 
@@ -218,3 +275,26 @@ function cascade(array, forEachLikeFunc, reverse=false) {
 		}
 	}, 100)
 }
+
+// cleanup input
+function cleanInput(word) {
+	const cleanWord = word.toLowerCase.trim()
+	return cleanWord
+}
+
+// event listeners
+gifContainers.forEach(container => {
+	container.addEventListener("click", (event) => {
+		const clickedContainer = event.target
+
+		if (clickedContainer.id === "play-btn") {
+			startGame()
+		}
+	})
+})
+
+guessform.addEventListener("submit", (event) => {
+	event.preventDefault()
+	let guess = cleanInput(guessInput.value)
+	
+})
